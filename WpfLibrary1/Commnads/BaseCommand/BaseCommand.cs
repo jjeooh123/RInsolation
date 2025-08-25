d74aaf35a537af8849eb3c876456f5e75cd3604f
1 <1@>
2025-08-25 11:48:28 +0000

    A wrapper base-class for updating the global context.

---
+++ WpfLibrary1/Commnads/BaseCommand/BaseCommand.cs
@@ -0,0 +1,37 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Insolation.ElementExtractor;
+
+namespace Insolation.Commnads
+{
+    /// <summary>
+    /// A base wrapper class that updates the global context 
+    /// before actually executing client logic.
+    /// </summary>
+    /// <remarks>
+    /// All commands must be wrapped in this shell, implement verification (for example, through reflection).
+    /// </remarks>
+    public abstract class BaseCommand : IExternalCommand
+    {
+        /// <summary>
+        /// Provides access to factories and shared services required by the command.
+        /// Default implementation uses <see cref="DefaultElementExtractionCommandServiceProvider"/>.
+        /// </summary>
+        public virtual BaseCommanderviceProviderBase ServiceProvider { get; set; }
+            = new DefaultBaseCommanderviceProvider();
+
+        /// <summary>
+        /// Standard Revit IExternalCommand entry point.
+        /// </summary>
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            ServiceProvider.GetIGlobalContextManager().SubmitResult(SharedContextKeys.ExternalCommandData, commandData);
+            return Logic(commandData, ref message, elements);
+        }
+
+        /// <summary>
+        /// Client Logic
+        /// </summary>
+        protected abstract Result Logic(ExternalCommandData commandData, ref string message, ElementSet elements);
+    }
+}
---
+++ WpfLibrary1/Commnads/BaseCommand/ContextProviders/DefaultBaseCommanderviceProvider.cs
@@ -0,0 +1,8 @@
+﻿namespace Insolation.Commnads
+{
+    /// <summary>
+    /// Default implementation of <see cref="BaseCommanderviceProviderBase"/>.
+    /// Delegates all resolutions to base class, based on <see cref="ServiceLocator"/>.
+    /// </summary>
+    public class DefaultBaseCommanderviceProvider : BaseCommanderviceProviderBase;
+}
